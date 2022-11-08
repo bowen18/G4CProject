32ef157f73540bc0ff5493af7d0838d05bb41c11
FubixDragon <90860075+FubixDragon@users.noreply.github.com>
2022-11-07 19:44:22 -0800

    Door pressure plate upgrade

--- G4CProject/Assets/Scripts/PressurePlate.cs
+++ G4CProject/Assets/Scripts/PressurePlate.cs
@@ -4,13 +4,43 @@ using UnityEngine;
 
 public class PressurePlate : MonoBehaviour
 {
-    [SerializeField]
-    GameObject door;
+    [SerializeField] Transform MovedObject;
 
+    [SerializeField] Transform[] MovePoints;
+    private int nextPoint = 0;
+    
+    public float moveForce = 1f;
+
+    private Vector3 EndPoint;
+    private Vector3 speed = Vector3.zero;
+
+
+    private void Start()
+    {
+        // sets the beginning EndPoint to the position of the object to be moved 
+        // so the object doesnt move to the first EndPoint in the MovePoints[] array
+        // on the start of the scene
+        EndPoint = MovedObject.position;
+    }
+    private void Update()
+    {
+        // moves "MovedObject" to desited location at "EndPoint"
+        MovedObject.position = Vector3.SmoothDamp(MovedObject.position, EndPoint, ref speed, moveForce); 
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        door.transform.position += new Vector3(0, 0, -3);
-        Debug.Log("Triggered");
+        Debug.Log("Triggerer");
+
+        // makes "EndPoint" the desired point to move to
+        // "MovePoints[]" is a collection of GameObjects positions
+        EndPoint = MovePoints[nextPoint].position;
+
+        // get the next MovePoint in the array
+        nextPoint += 1;
+
+        //if the last EndPoint has been reached, start from the beginning 
+        if (nextPoint > MovePoints.Length - 1) nextPoint = 0;
+
     }
 }
